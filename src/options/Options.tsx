ef4f7214e35d0951ce1dece4826106be8cec8c9e
mosade <yout72495@gmail.com>
2024-05-19 15:31:25 +0800

    init

---
+++ src/options/Options.tsx
@@ -0,0 +1,14 @@
+import { General } from '@/options/component/general'
+
+export const Options = () => {
+
+
+  return (
+    <main>
+      <General></General>
+    </main>
+  )
+}
+
+
+export default Options
