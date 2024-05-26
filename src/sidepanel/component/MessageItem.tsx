2940660cced0847d7ad2d6ec2d0e5d11583a70b2
mosade <yout72495@gmail.com>
2024-05-26 21:42:12 +0800

    feat: modify message style

--- src/sidepanel/component/MessageItem.tsx
+++ src/sidepanel/component/MessageItem.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardTitle } from '@/components/ui/card'
 import markdownit from 'markdown-it'
 import { useEffect, useRef } from 'react'
 import { Skeleton } from '@/components/ui/skeleton'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 
 export function MessageItem(props: { message: Message }) {
   const { message } = props
@@ -11,17 +12,21 @@ export function MessageItem(props: { message: Message }) {
     mdRef.current = markdownit()
   }, [])
   const markDownRenderedHtml = mdRef.current?.render(message.content)
-  return <Card>
-    <CardTitle>{message.character}</CardTitle>
-    <CardContent>
-      {markDownRenderedHtml ?
-        <div dangerouslySetInnerHTML={{ __html: markDownRenderedHtml }}></div>
-        :
-        <div className="flex flex-col gap-2">
-          <Skeleton className="h-4 w-[150px]" />
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[300px]" />
-        </div>}
-    </CardContent>
-  </Card>
+  return <div className="flex gap-2">
+    <Avatar>
+      <AvatarFallback>{message.character}</AvatarFallback>
+    </Avatar>
+    <Card className="flex-1">
+      <CardContent>
+        {markDownRenderedHtml ?
+          <div dangerouslySetInnerHTML={{ __html: markDownRenderedHtml }}></div>
+          :
+          <div className="flex flex-col gap-2">
+            <Skeleton className="h-4 w-[150px]" />
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[300px]" />
+          </div>}
+      </CardContent>
+    </Card>
+  </div>
 }
