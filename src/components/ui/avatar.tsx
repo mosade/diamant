15064ae2816a01c8829a077623a31d300467a026
mosade <yout72495@gmail.com>
2024-05-27 22:31:20 +0800

    feat: dark theme

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -37,7 +37,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-slate-200",
+      "flex h-full w-full items-center justify-center rounded-full bg-slate-200 dark:bg-slate-400",
       className
     )}
     {...props}
