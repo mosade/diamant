adaebdff5e60e1ccc13576dc17f2636247c6cd69
mosade <384466872@qq.com>
2024-07-24 13:08:40 +0800

    feat: improve message display with smooth scrolling

--- src/sidepanel/component/MessageList.tsx
+++ src/sidepanel/component/MessageList.tsx
@@ -11,11 +11,25 @@ export function MessageList(props: { messages: Message[] }) {
   const messageListRef = useRef<HTMLDivElement>(null)
   const scrollContentRef = useRef<HTMLDivElement>(null)
 
+  /**
+   * 判断是否在底部，如果不在底部则滚动到底部\
+   * TODO 只是在生成内容的时候触发滚动
+   */
+  function isInBottomInRange() {
+    const scrollHeight = scrollContentRef.current?.scrollHeight ?? 0
+    const scrollTop = scrollContentRef.current?.scrollTop ?? 0
+    const clientHeight = scrollContentRef.current?.clientHeight ?? 0
+    console.log(scrollHeight - scrollTop - clientHeight);
+    if (scrollHeight - scrollTop - clientHeight <= 50) {
+      messageListRef.current?.scrollIntoView({ behavior: 'smooth', block: 'end' })
+    }
+  }
+  isInBottomInRange()
 
   return <ScrollArea ref={scrollContentRef} className="h-full w-full rounded-md border p-4">
     <div className="flex flex-col gap-4" ref={messageListRef}>
       {displayedMessages.map((message, index) =>
-        (<MessageItem  message={message} key={index}></MessageItem>
+        (<MessageItem message={message} key={index}></MessageItem>
         ))
       }
     </div>
